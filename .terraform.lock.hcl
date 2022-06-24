7c1d1f7b50501ba0bb0d0149aea373e61e8616d9
JB Beck <jb.beck@icloud.com>
2022-06-24 10:10:31 +0200

    initial commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,20 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.scalr.io/scalr/scalr" {
+  version     = "1.0.0-rc30"
+  constraints = "1.0.0-rc30"
+  hashes = [
+    "h1:Uesd0/CRpGjpWgQ3qxAlBcahjCPuProMZHzA0EV5/qI=",
+    "zh:26a4c437488d8ab81183d0d46f2c013f2b1fadc19909267eda4172b903357547",
+    "zh:3d99009a41554db352a055770199aaf45625fc0b5f92fe56b1a1e60351e17b10",
+    "zh:45243a0891a56f33bdcbccfca76bb4f5621f133ee71576a8b072d0b8faa3edac",
+    "zh:6d61e18ed6489c95b3c05d9bb94d06774dc978594cde905e2fa3601b456f4a6f",
+    "zh:6d6555c250160d792213dda14a56f35338e9ecc5c1cce02569252b5e5176bd4f",
+    "zh:6fe994ad2bfb7e81b1f1ef737cb1b9608b48d9fee86733b3f166c9450526ee9f",
+    "zh:7b9fbd0bbe15753ac66f55fafc8809e8bebec769f5129eea868cffc5b5411c22",
+    "zh:95333b22e0a11ae1900efe1f93896fd3ae28934ff28f7568e7c26b5045b0eaf6",
+    "zh:ba6827df323f89775c1fe5ecc95cf48196b114f8693d4e80b9f823fb862629b0",
+    "zh:c8b8e8290caa9d80d9c8348024ac774d72ebf7efc717f8fbb8282bce55c5454e",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,57 @@
+resource "scalr_role" "planner" {
+  name        = "Plan Creation"
+  account_id  = local.account_id
+  description = "Ability to create Terraform runs, but not approve"
+
+  permissions = [
+    "plans:read-json-output",
+    "runs:cancel",
+    "runs:create",
+    "workspaces:read",
+    "workspaces:set-schedule",
+    "workspaces:update"
+  ]
+}
+
+locals {
+  account_id     = "acc-sscctbisjkl35b8"
+  environment_id = "env-u0b83rvjmsjkov8"
+}
+
+
+resource "scalr_iam_team" "example" {
+  name        = "myfirstscalrteam"
+  description = "Scalr Example Team"
+  account_id  = local.account_id
+}
+
+resource "scalr_access_policy" "team_min_access_to_acc_scope" {
+  subject {
+    type = "team"
+    id   = scalr_iam_team.example.id
+  }
+  scope {
+    type = "account"
+    id   = local.account_id
+  }
+  ##This is a system role and the role ID will not change per account##
+  role_ids = [
+    "role-t58u316skvrgk2g"
+  ]
+}
+
+##Link to an environment with role created earlier##
+resource "scalr_access_policy" "team_access_to_env_scope" {
+  subject {
+    type = "team"
+    id   = scalr_iam_team.example.id
+  }
+  scope {
+    type = "environment"
+    id   = local.environment_id
+  }
+
+  role_ids = [
+    scalr_role.planner.id
+  ]
+}
